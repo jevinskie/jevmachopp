4210e937a3a68972d9b816384fea2363ace2f5ff
Jevin Sweval <jevinsweval@gmail.com>
2021-11-14 23:08:11 -0800

    wip

--- lib/jevmachopp/LoadSubCommand.cpp
+++ lib/jevmachopp/LoadSubCommand.cpp
@@ -1,10 +1,20 @@
 #include "jevmachopp/LoadCommand.h"
 #include "jevmachopp/LoadSubCommand.h"
+#include "jevmachopp/LoadCommandType.h"
 
 const LoadCommand *LoadSubCommand::loadCommand() const {
 	return (const LoadCommand *)((uintptr_t)this - sizeof(LoadCommand));
 }
 
 const SubCommandVariant LoadSubCommand::get() const {
-	return SubCommandVariant{(const EncryptionInfoCommand*)nullptr};
+	switch (LoadCommand().cmd) {
+	case LoadCommandType::ENCRYPTION_INFO:
+		return SubCommandVariant{(const EncryptionInfoCommand*)this};
+	case LoadCommandType::SEGMENT_64:
+		return SubCommandVariant{(const SegmentCommand*)this};
+	case LoadCommandType::UUID:
+		return SubCommandVariant{(const UUIDCommand*)this};
+	default:
+		return SubCommandVariant{(const UnknownCommand*)this};
+	}
 }
