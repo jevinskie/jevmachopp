206d67ad797231edcaf0c7775c32d4f49b869d78
Jevin Sweval <jevinsweval@gmail.com>
2021-11-15 20:26:43 -0800

    wip

--- include/jevmachopp/UUIDCommand.h
+++ include/jevmachopp/UUIDCommand.h
@@ -50,14 +50,5 @@ template <> struct fmt::formatter<UUIDCommand> {
             ++p;
         }
         return out;
-        // return fmt::format_to(ctx.out(),
-        //                       "<UUID @ {:p} "
-        //                       "{:02x}{:02x}{:02x}{:02x}-{:02x}{:02x}-{:02x}{:02x}-{:02x}{:02x}-{:"
-        //                       "02x}{:02x}{:02x}{:02x}{:02x}{:02x}>"_cf,
-        //                       (void *)this, uuid.uuid[0], uuid.uuid[1], uuid.uuid[2],
-        //                       uuid.uuid[3], uuid.uuid[4], uuid.uuid[5], uuid.uuid[6],
-        //                       uuid.uuid[7], uuid.uuid[8], uuid.uuid[9], uuid.uuid[10],
-        //                       uuid.uuid[11], uuid.uuid[12], uuid.uuid[13], uuid.uuid[14],
-        //                       uuid.uuid[15]);
     }
 };
