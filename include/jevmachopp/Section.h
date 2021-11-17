bb83ceac6e4fa8f59f2d6e655ee4eb89a7dedec9
Jevin Sweval <jevinsweval@gmail.com>
2021-11-16 23:52:36 -0800

    wip

--- include/jevmachopp/Section.h
+++ include/jevmachopp/Section.h
@@ -12,6 +12,11 @@ public:
     void operator=(const Section &) = delete;
 
 public:
+    std::string sectName() const;
+    std::string segName() const;
+    fmt::appender &format_to(fmt::appender &out) const;
+
+public:
     char sectname[16];
     char segname[16];
     uint64_t addr;
@@ -27,3 +32,15 @@ public:
 };
 
 static_assert_size_same(Section, struct section_64);
+
+template <> struct fmt::formatter<Section> {
+
+    template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
+        return ctx.begin();
+    }
+
+    template <typename FormatContext> auto format(Section const &sect, FormatContext &ctx) {
+        auto out = ctx.out();
+        return sect.format_to(out);
+    }
+};
--- lib/jevmachopp/Section.cpp
+++ lib/jevmachopp/Section.cpp
@@ -1,1 +1,14 @@
 #include "jevmachopp/Section.h"
+
+std::string Section::sectName() const {
+    return readMaybeNullTermCString<decltype(sectname)>(sectname);
+}
+
+std::string Section::segName() const {
+    return readMaybeNullTermCString<decltype(segname)>(segname);
+}
+
+fmt::appender &Section::format_to(fmt::appender &out) const {
+    fmt::format_to(out, "<Section @ {:p} {:s},{:s}>"_cf, (void *)this, segName(), sectName());
+    return out;
+}
