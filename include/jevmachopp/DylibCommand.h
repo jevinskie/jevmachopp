592bc0fb8f2dc1576b86d5268371372727f70428
Jevin Sweval <jevinsweval@gmail.com>
2021-11-17 13:47:59 -0800

    DylibCommand name() works

--- include/jevmachopp/DylibCommand.h
+++ include/jevmachopp/DylibCommand.h
@@ -2,14 +2,23 @@
 
 #include "jevmachopp/Common.h"
 #include "jevmachopp/LoadSubCommand.h"
+#include <mach-o/loader.h>
+#include <string>
 
 class DylibCommand : public LoadSubCommand {
 public:
-    DylibCommand(const DylibCommand &) = delete;
-    void operator=(const DylibCommand &) = delete;
+    const char *name() const;
+    fmt::appender &format_to(fmt::appender &out) const;
 
 public:
-    uint32_t dummy[4];
+    uint32_t name_offset;
+    uint32_t timestamp;
+    uint32_t current_version;
+    uint32_t compatibility_version;
+
+public:
+    DylibCommand(const DylibCommand &) = delete;
+    void operator=(const DylibCommand &) = delete;
 };
 
 static_assert_size_same_minus_header(DylibCommand, struct dylib_command, struct load_command);
@@ -23,8 +32,6 @@ template <> struct fmt::formatter<DylibCommand> {
     template <typename FormatContext>
     auto format(DylibCommand const &dylibCmd, FormatContext &ctx) {
         auto out = ctx.out();
-        fmt::format_to(out, "<DylibCommand @ {:p} \"{:s}\">"_cf, (void *)dylibCmd.loadCommand(),
-                       "todo.dylib");
-        return out;
+        return dylibCmd.format_to(out);
     }
 };
--- lib/jevmachopp/DylibCommand.cpp
+++ lib/jevmachopp/DylibCommand.cpp
@@ -1,1 +1,13 @@
 #include "jevmachopp/DylibCommand.h"
+
+const char *DylibCommand::name() const {
+    return (const char *)((uintptr_t)loadCommand() + name_offset);
+}
+
+fmt::appender &DylibCommand::format_to(fmt::appender &out) const {
+    fmt::format_to(
+        out,
+        "<DylibCommand @ {:p} \"{:s}\" timestamp: {:#010x} current_version: {:#010x} compatibility_version: {:#010x}>"_cf,
+        (void *)loadCommand(), name(), timestamp, current_version, compatibility_version);
+    return out;
+}
