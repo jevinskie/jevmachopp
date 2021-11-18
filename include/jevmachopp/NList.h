41ab459a628df36665c9c966a2593fda6175ed4f
Jevin Sweval <jevinsweval@gmail.com>
2021-11-18 00:45:53 -0800

    broken shit

--- include/jevmachopp/NList.h
+++ include/jevmachopp/NList.h
@@ -24,6 +24,25 @@ public:
 
 static_assert_size_same(NList, struct nlist_64);
 
+// // Checks if an argument is a valid printf width specifier and sets
+// // left alignment if it is negative.
+template <typename Char> class my_handler {
+
+ public:
+  explicit my_handler() {}
+
+
+  template <typename T, std::enable_if_t<std::is_integral<T>::value>>
+  unsigned operator()(T value) {
+    return static_cast<unsigned>(value);
+  }
+
+  template <typename T, std::enable_if_t<!std::is_integral<T>::value>>
+  unsigned operator()(T) {
+    return 0;
+  }
+};
+
 template <> struct fmt::formatter<NList> {
     // Presentation format: 'd' - dumb, 'f' - fancy.
     char presentation = 'd';
@@ -53,11 +72,30 @@ template <> struct fmt::formatter<NList> {
             auto macho_arg = ctx.arg(macho_arg_id);
             const MachO *macho_ptr = nullptr;
             visit_format_arg(
-                [](auto v) {
-//                    auto v = custom.value;
+                [=](auto val) {
+                    using T = decltype(val);
+                    if (std::is_same_v<T, int>) {
+                        fmt::print("int\n");
+                    }
+                    if (std::is_same_v<T, const void *>) {
+                        fmt::print("pointer\n");
+                    }
+                    if (std::is_same_v<T, fmt::basic_format_arg<format_context>::handle>) {
+                        fmt::print("macho??\n");
+                        // auto v = reinterpret_cast<const void*>(val);
+                        // auto v = fmt::custom_value<format_context>(val);
+                        // auto vz = detail::value<decltype(ctx)>{};
+                        // auto v = detail::arg_mapper<decltype(ctx)>::map();
+                        fmt::print("fark\n");
+                        // auto v = static_cast<fmt::basic_format_arg<format_context>::handle>(val);
+                    }
+                    //                    auto v = custom.value;
                     fmt::print("fuck\n");
                 },
+                // my_handler(),
                 macho_arg);
+
+            auto bar = visit_format_arg(my_handler<char>(), macho_arg);
             fmt::print("");
         }
         return nlist.format_to(out);
