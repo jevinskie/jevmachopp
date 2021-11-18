82fc1a249757a5dbc8c560f9539d8a6dd12e85aa
Jevin Sweval <jevinsweval@gmail.com>
2021-11-17 21:49:36 -0800

    fmt magic wip

--- include/jevmachopp/NList.h
+++ include/jevmachopp/NList.h
@@ -24,13 +24,25 @@ public:
 static_assert_size_same(NList, struct nlist_64);
 
 template <> struct fmt::formatter<NList> {
+    // Presentation format: 'd' - dumb, 'f' - fancy.
+    char presentation = 'd';
 
-    constexpr auto parse(format_parse_context &ctx) {
-        return ctx.begin();
+    constexpr auto parse(format_parse_context &ctx) -> decltype(ctx.begin()) {
+        auto it = ctx.begin(), end = ctx.end();
+        if (it != end && (*it == 'd' || *it == 'f'))
+            presentation = *it++;
+
+        // Check if reached the end of the range:
+        if (it != end && *it != '}')
+            throw format_error("invalid format");
+
+        // Return an iterator past the end of the parsed range:
+        return it;
     }
 
-    auto format(NList const &nlist, format_context &ctx) {
+    auto format(NList const &nlist, format_context &ctx) -> decltype(ctx.out()) {
         auto out = ctx.out();
+        fmt::print("\nWHATWHAT ctx: {} THATSWHAT\n", presentation);
         return nlist.format_to(out);
     }
 };
