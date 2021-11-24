e1953bcae48066b288499b82cc65269e4de5fd87
Jevin Sweval <jevinsweval@gmail.com>
2021-11-24 14:02:48 -0800

    meh

--- include/jevmachopp/NVRAM.h
+++ include/jevmachopp/NVRAM.h
@@ -2,18 +2,22 @@
 
 #include "jevmachopp/Common.h"
 
-typedef struct chrp_nvram_header { //16 bytes
-	uint8_t  sig;
-	uint8_t  cksum; // checksum on sig, len, and name
-	uint16_t len;   // total length of the partition in 16 byte blocks starting with the signature
-	// and ending with the last byte of data area, ie len includes its own header size
-	char     name[12];
-	uint8_t  data[0];
+#include <experimental/fixed_capacity_vector>
+
+typedef struct chrp_nvram_header { // 16 bytes
+    uint8_t sig;
+    uint8_t cksum; // checksum on sig, len, and name
+    uint16_t len;  // total length of the partition in 16 byte blocks starting with the signature
+    // and ending with the last byte of data area, ie len includes its own header size
+    char name[12];
+    uint8_t data[0];
 } chrp_nvram_header_t;
 
-typedef struct apple_nvram_header {  // 16 + 16 bytes
-	struct   chrp_nvram_header chrp;
-	uint32_t adler;
-	uint32_t generation;
-	uint8_t  padding[8];
+typedef struct apple_nvram_header { // 16 + 16 bytes
+    struct chrp_nvram_header chrp;
+    uint32_t adler;
+    uint32_t generation;
+    uint8_t padding[8];
 } apple_nvram_header_t;
+
+std::size_t escapeDataToData(const std::span<const uint8_t> &value, std::span<uint8_t> &decoded);
--- lib/jevmachopp/NVRAM.cpp
+++ lib/jevmachopp/NVRAM.cpp
@@ -1,5 +1,41 @@
 #include "jevmachopp/NVRAM.h"
+#include "jevmachopp/Hex.h"
 #include "jevmachopp/c/jevnvram.h"
+
+#pragma mark Environment Variable Codings
+
+std::size_t escapeDataToData(const std::span<const uint8_t> &value, std::span<uint8_t> &&decoded) {
+    const auto dec_buf_sz = decoded.size_bytes();
+    const auto enc_buf_sz = value.size_bytes();
+    assert(dec_buf_sz >= enc_buf_sz);
+
+    const uint8_t *startPtr;
+    const uint8_t *endPtr;
+    const uint8_t *wherePtr;
+    std::size_t outOff = 0;
+    uint8_t byte;
+    bool ok = true;
+
+    wherePtr = (const uint8_t *)value.data();
+    endPtr = wherePtr + value.size_bytes();
+
+    while (wherePtr < endPtr) {
+        startPtr = wherePtr;
+        byte = *wherePtr++;
+        if ((byte == 0x00) || (byte == 0xFF)) {
+            for (; ((wherePtr - startPtr) < 0x7F) && (wherePtr < endPtr) && (byte == *wherePtr);
+                 wherePtr++) {}
+            decoded[outOff++] = 0xff;
+            byte = (byte & 0x80) | ((uint8_t)(wherePtr - startPtr));
+        }
+        decoded[outOff++] = byte;
+    }
+    decoded[outOff++] = 0;
+
+    assert(outOff <= decoded.size_bytes());
+
+    return outOff;
+}
 
 #pragma mark C
 
@@ -7,6 +43,21 @@ void dump_nvram(const void *nvram_buf) {
     printf("nvram @ %p\n", nvram_buf);
 
 #if USE_FMT
+
+    // const auto buf_span = std::span<const uint8_t>{(const uint8_t*)nvram_buf, 32};
+    const auto buf_span = std::span<const uint8_t>{(const uint8_t *)nvram_buf, 423};
+    std::experimental::fixed_capacity_vector<uint8_t, 512> out_vec{};
+    fmt::print("buf_span: size: {:d} size_bytes: {:d}\n", buf_span.size(), buf_span.size_bytes());
+    fmt::print("buf_span: extent: {:d}\n", buf_span.extent);
+    fmt::print("buf_span: Extent: {:d}\n", decltype(buf_span)::extent);
+    // std::span<uint8_t> out_vec_span{out_vec.begin(), out_vec.end()};
+    // auto decoded_sz = escapeDataToData(buf_span, out_vec_span);
+    auto decoded_sz = escapeDataToData(
+        buf_span, std::span<uint8_t>{out_vec.begin(), out_vec.begin() + out_vec.capacity()});
+    fmt::print("decoded_sz: {:d} out_vec.size(): {:d}\n", decoded_sz, out_vec.size());
+
+    auto hexstr = buf2hexstr<512>(out_vec.data(), decoded_sz);
+    fmt::print("hexstr: {:s}\n", hexstr);
 
     fmt::print("nvram: {:p}\n", nvram_buf);
 
