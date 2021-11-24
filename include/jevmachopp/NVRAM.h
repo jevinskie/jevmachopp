4002213a337a15c6a60ccaf0a7d3aa4c30a64e91
Jevin Sweval <jevinsweval@gmail.com>
2021-11-24 12:59:16 -0800

    fix(ish) apple-nvram.bt

--- include/jevmachopp/NVRAM.h
+++ include/jevmachopp/NVRAM.h
@@ -1,3 +1,19 @@
 #pragma once
 
 #include "jevmachopp/Common.h"
+
+typedef struct chrp_nvram_header { //16 bytes
+	uint8_t  sig;
+	uint8_t  cksum; // checksum on sig, len, and name
+	uint16_t len;   // total length of the partition in 16 byte blocks starting with the signature
+	// and ending with the last byte of data area, ie len includes its own header size
+	char     name[12];
+	uint8_t  data[0];
+} chrp_nvram_header_t;
+
+typedef struct apple_nvram_header {  // 16 + 16 bytes
+	struct   chrp_nvram_header chrp;
+	uint32_t adler;
+	uint32_t generation;
+	uint8_t  padding[8];
+} apple_nvram_header_t;
