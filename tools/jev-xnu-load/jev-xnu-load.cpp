6eecbcc889a0d3c97e15b3fdc77b8b1873a3a667
Jevin Sweval <jevinsweval@gmail.com>
2021-11-26 11:19:42 -0800

    jev-xnu-load options, exception fixes for static_string, add cxxopts submodule, only build libjevmachopp without excpetions, let tools use them

--- tools/jev-xnu-load/jev-xnu-load.cpp
+++ tools/jev-xnu-load/jev-xnu-load.cpp
@@ -1,12 +1,32 @@
-#include <assert.h>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+
+#include <cxxopts.hpp>
 
 #include <jevmachopp/Slurp.h>
 #include <jevmachopp/c/jevxnuboot.h>
 
 int main(int argc, const char *argv[]) {
-    assert(argc == 2);
-    uint64_t size;
-    uint8_t *inbuf = Slurp::readfile(argv[1], &size);
+    cxxopts::Options options("jev-xnu-load",
+                             "Mock iBoot prep of KC and other data then mock chainloading xnu");
+
+    // clang-format off
+    options.add_options()
+        ("k,kernelcache", "kernelcache file", cxxopts::value<std::string>())
+        ("s,sepfw", "SEPFW file", cxxopts::value<std::string>())
+        ("b,boot-args", "BootArgs file", cxxopts::value<std::string>())
+        ("d,devicetree", "DeviceTree file", cxxopts::value<std::string>())
+        ("t,trustcache", "TrustCache file", cxxopts::value<std::string>())
+        ("h,help", "Print usage")
+    ;
+    // clang-format on
+
+    auto args = options.parse(argc, argv);
+    if (args.count("help")) {
+        printf("%s\n", options.help().data());
+        exit(0);
+    }
 
     const void *entry_point = XNUBoot::load_and_prep_xnu_kernelcache();
     printf("entry_point: %p\n", entry_point);
