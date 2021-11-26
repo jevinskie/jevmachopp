a5ac14a233122ee83d35403b53a75cc6b0166a98
Jevin Sweval <jevinsweval@gmail.com>
2021-11-26 10:19:19 -0800

    XNUBootArgs skeleton

--- include/jevmachopp/XNUBoot.h
+++ include/jevmachopp/XNUBoot.h
@@ -2,3 +2,40 @@
 
 #include "jevmachopp/Common.h"
 #include "jevmachopp/c/jevxnuboot.h"
+
+namespace XNUBoot {
+
+constexpr auto BOOT_LINE_LENGTH = 608;
+
+} // namespace XNUBoot
+
+class XNUBootArgsVideo {
+public:
+    void *baseAddr;    // Base address of video memory
+    uint64_t display;  // Display Code (if Applicable)
+    uint64_t rowBytes; // Number of bytes per pixel row
+    uint64_t width;    // Width
+    uint64_t height;   // Height
+    uint64_t depth;    // Pixel Depth and other parameters
+};
+
+static_assert_size_is(XNUBootArgsVideo, 48);
+
+class XNUBootArgs {
+public:
+    uint16_t revision;         // Revision of boot_args structure
+    uint16_t version;          // Version of boot_args structure
+    uint64_t virtBase;         // Virtual base of memory
+    uint64_t physBase;         // Physical base of memory
+    uint64_t memSize;          // Size of memory
+    uint64_t topOfKernelData;  // Highest physical address used in kernel data area
+    XNUBootArgsVideo video;    // Video Information
+    uint32_t machineType;      // Machine Type
+    void *deviceTree;          // Base of flattened device tree
+    uint32_t deviceTreeLength; // Length of flattened tree
+    char commandLine[XNUBoot::BOOT_LINE_LENGTH]; // Passed in command line
+    uint64_t bootFlags;                          // Additional flags specified by the bootloader
+    uint64_t memSizeActual;                      // Actual size of memory
+};
+
+static_assert_size_is(XNUBootArgs, 736);
