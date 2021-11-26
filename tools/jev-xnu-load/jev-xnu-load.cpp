4b931537ecb037105ca4835316adf4f8f3e22adf
Jevin Sweval <jevinsweval@gmail.com>
2021-11-26 13:43:43 -0800

    wip

--- tools/jev-xnu-load/jev-xnu-load.cpp
+++ tools/jev-xnu-load/jev-xnu-load.cpp
@@ -7,6 +7,31 @@
 #include <jevmachopp/Slurp.h>
 #include <jevmachopp/c/jevxnuboot.h>
 
+// clang-format off
+constexpr uintptr_t virt_base     = 0xffff'fe00'19cb'c000;
+constexpr uintptr_t phys_base     = 0x0000'0008'01cb'c000;
+constexpr uintptr_t virt_phys_off = 0xffff'fdf8'1800'0000;
+
+constexpr uintptr_t dt_arg_base   = 0xffff'fe00'1bc6'4000;
+constexpr uintptr_t dt_base       = 0x0000'0008'03c6'4000;
+constexpr uintptr_t dt_end        = 0x0000'0008'03cb'c000;
+
+constexpr uintptr_t tc_base       = 0x0000'0008'03cb'c000;
+constexpr uintptr_t tc_end        = 0x0000'0008'03d1'c000;
+
+constexpr uintptr_t m1n1_base     = 0x0000'0008'03d1'c000;
+
+constexpr uintptr_t kc_base       = 0x0000'0008'03d1'c000;
+constexpr uintptr_t entry_point   = 0x0000'0008'0463'8530;
+constexpr uintptr_t kc_end        = 0x0000'0008'092c'c000;
+
+constexpr uintptr_t sepfw_base    = 0x0000'0008'081b'4000;
+constexpr uintptr_t sepfw_end     = 0x0000'0008'088d'c000;
+
+constexpr uintptr_t ba_base       = 0x0000'0008'088d'c000;
+constexpr uintptr_t ba_end        = 0x0000'0008'088e'0000;
+// clang-format on
+
 int main(int argc, const char *argv[]) {
     cxxopts::Options options("jev-xnu-load",
                              "Mock iBoot prep of KC and other data then mock chainloading xnu");
@@ -23,13 +48,14 @@ int main(int argc, const char *argv[]) {
     // clang-format on
 
     auto args = options.parse(argc, argv);
-    if (args.count("help")) {
+    if (args.count("help") || !args.count("kernelcache") || !args.count("sepfw") ||
+        !args.count("boot-args") || !args.count("devicetree") || !args.count("trustcache")) {
         printf("%s\n", options.help().data());
-        exit(0);
+        return 1;
     }
 
-    const void *entry_point = XNUBoot::load_and_prep_xnu_kernelcache();
-    printf("entry_point: %p\n", entry_point);
+    const void *calculated_entry_point = XNUBoot::load_and_prep_xnu_kernelcache();
+    printf("calculated_entry_point: %p\n", calculated_entry_point);
 
     return 0;
 }
