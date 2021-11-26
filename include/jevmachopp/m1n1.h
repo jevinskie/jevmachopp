30df864d7e0f3176bff45d1abe54aac8a326d456
Jevin Sweval <jevinsweval@gmail.com>
2021-11-25 17:34:32 -0800

    wip

--- include/jevmachopp/m1n1.h
+++ include/jevmachopp/m1n1.h
@@ -4,6 +4,4 @@
 
 extern "C" __attribute__((noreturn)) void xnu_jump_stub(uint64_t new_base_plus_bootargs_off,
                                                         uint64_t image_addr, uint64_t new_base,
-                                                        uint64_t image_size, uint64_t image_entry) {
-
-}
+                                                        uint64_t image_size, uint64_t image_entry);
