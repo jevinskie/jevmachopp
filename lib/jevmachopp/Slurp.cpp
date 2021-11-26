452f79feda28fa87c596b0806c03615a497de74e
Jevin Sweval <jevinsweval@gmail.com>
2021-11-26 10:43:12 -0800

    Slurp improvements, add Slurp::mapMemory()

--- lib/jevmachopp/Slurp.cpp
+++ lib/jevmachopp/Slurp.cpp
@@ -8,31 +8,45 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-uint8_t *Slurp::readfile(const char *filename, uint64_t *len, void *preferred_addr) {
-    int fd;
-    struct stat st;
-    uint8_t *buf;
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
+uint8_t *Slurp::readfile(const char *filename, uint64_t *len, bool rw, void *preferred_addr) {
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0) {
         assert(!"couldnt open");
-    if (fstat(fd, &st) != 0)
+    }
+
+    struct stat st {};
+    if (fstat(fd, &st) != 0) {
         assert(!"couldnt stat");
+    }
 
-    *len = (uint64_t)st.st_size;
-
-    buf = (uint8_t *)mmap(preferred_addr, st.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-    if (buf == NULL)
+    auto *buf = (uint8_t *)mmap(preferred_addr, st.st_size, rw ? PROT_READ | PROT_WRITE : PROT_READ,
+                                MAP_PRIVATE | (preferred_addr ? MAP_FIXED : 0), fd, 0);
+    if (buf == NULL) {
         assert(!"couldnt mmap");
+    }
     close(fd);
+
+    if (len) {
+        *len = (uint64_t)st.st_size;
+    }
 
     return buf;
 }
 
 void Slurp::writefile(const char *filename, uint8_t *buf, uint64_t len) {
-    int fd;
-
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0755);
-    write(fd, buf, len);
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0755);
+    assert(fd >= 0);
+    auto wrote_len = write(fd, buf, len);
+    assert(wrote_len == (ssize_t)len);
     close(fd);
+}
+
+uint8_t *Slurp::mapMemory(std::size_t size, void *preferred_addr) {
+    auto *buf =
+        (uint8_t *)mmap(preferred_addr, size, PROT_READ | PROT_WRITE,
+                        MAP_ANONYMOUS | MAP_PRIVATE | (preferred_addr ? MAP_FIXED : 0), -1, 0);
+    if (buf == NULL) {
+        assert(!"couldnt mmap");
+    }
+    return buf;
 }
