7a73563aed0c9aa8a43105354b072eaefd7111bd
Jevin Sweval <jevinsweval@gmail.com>
2021-11-27 14:31:23 -0800

    m1n1 compatible

--- include/jevmachopp/c/jevxnuboot.h
+++ include/jevmachopp/c/jevxnuboot.h
@@ -11,6 +11,10 @@ namespace XNUBoot {
 void load_and_prep_xnu_kernelcache(const void *boot_args_base, const void *xnu_jump_stub_ptr,
                                    size_t stub_size);
 
+__attribute__((noreturn)) void xnu_jump_stub(uint64_t new_base_plus_bootargs_off,
+                                             uint64_t image_addr, uint64_t new_base,
+                                             uint64_t image_size, uint64_t image_entry);
+
 #ifdef __cplusplus
 } // namespace XNUBoot
 #endif
--- include/jevmachopp/m1n1.h
+++ include/jevmachopp/m1n1.h
@@ -6,8 +6,4 @@ namespace m1n1 {
 
 void flush_i_and_d_cache(const void *addr, ssize_t size);
 
-extern "C" __attribute__((noreturn)) void xnu_jump_stub(uint64_t new_base_plus_bootargs_off,
-                                                        uint64_t image_addr, uint64_t new_base,
-                                                        uint64_t image_size, uint64_t image_entry);
-
 } // namespace m1n1
