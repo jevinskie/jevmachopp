e404090a96518af46855a1ce7994c961c2ef2cb3
Jevin Sweval <jevinsweval@gmail.com>
2021-11-26 19:08:54 -0800

    bad indent, bad

--- lib/jevmachopp/XNUBoot.cpp
+++ lib/jevmachopp/XNUBoot.cpp
@@ -23,47 +23,58 @@ const void *load_and_prep_xnu_kernelcache(const void *boot_args_base) {
     const auto *dt_phys_ptr = (const DTNode *)((uintptr_t)bootArgs.deviceTree - virtOff);
     const auto &dt = *dt_phys_ptr;
 
-    if (auto chosen_node_ptr = dt.childNamed("chosen")) {
-        auto &chosen_node = *chosen_node_ptr;
-
-        if (auto iuou_prop_ptr = chosen_node.propertyNamed("internal-use-only-unit")) {
-            auto &iuou_prop = *iuou_prop_ptr;
-            printf("iuou prop before: 0x%08x\n", iuou_prop.as_u32());
-            assert(iuou_prop.size_raw() == 4);
-            (uint32_t &)iuou_prop.as_u32() = 1;
-            printf("iuou prop after: 0x%08x\n", iuou_prop.as_u32());
-        } else {
-            printf("internal-use-only-unit prop not found in device tree\n");
-            // sad but not fatal
-            // return nullptr;
-        }
-
-        if (auto nvram_proxy_data_prop_ptr = chosen_node.propertyNamed("nvram-proxy-data")) {
-            auto &nvram_proxy_data_prop = *nvram_proxy_data_prop_ptr;
-            const auto proxyData = NVRAM::extractProxyData(nvram_proxy_data_prop.data());
-            const auto hasVerbose = proxyData.system_hasBootArg("-v");
-            printf("boot-args has -v flag: %d\n", hasVerbose);
-            if (!hasVerbose) {
-                printf("didn't detect verbose boot flag, bailing out of xnu load\n");
-                return nullptr;
-            }
-        }
-
-        if (auto memory_map_node_ptr = chosen_node.childNamed("memory-map")) {
-            auto &memory_map_node = *memory_map_node_ptr;
-            FMT_PRINT("memory_map_node: {}\n", memory_map_node);
-            for (const auto &map_region : memory_map_node.properties_sized(sizeof(DTRegister))) {
-                FMT_PRINT("map_region: {}\n", map_region);
-                FMT_PRINT("map_region.as_reg(): {}\n", map_region.as_reg());
-            }
-        } else {
-            printf("didn't detect verbose boot flag, bailing out of xnu load\n");
-            return nullptr;
-        }
-    } else {
+    auto chosen_node_ptr = dt.childNamed("chosen");
+    if (!chosen_node_ptr) {
         printf("chosen node not found in device tree\n");
         return nullptr;
     }
+    auto &chosen_node = *chosen_node_ptr;
+
+    if (auto iuou_prop_ptr = chosen_node.propertyNamed("internal-use-only-unit")) {
+        auto &iuou_prop = *iuou_prop_ptr;
+        printf("iuou prop before: 0x%08x\n", iuou_prop.as_u32());
+        assert(iuou_prop.size_raw() == 4);
+        (uint32_t &)iuou_prop.as_u32() = 1;
+        printf("iuou prop after: 0x%08x\n", iuou_prop.as_u32());
+    } else {
+        printf("internal-use-only-unit prop not found in device tree\n");
+        // sad but not fatal
+        // return nullptr;
+    }
+
+    auto nvram_proxy_data_prop_ptr = chosen_node.propertyNamed("nvram-proxy-data");
+    if (!nvram_proxy_data_prop_ptr) {
+        printf("couldn't find chosen/nvram-proxy-data to check for verbose boot flag, bailing out "
+               "of xnu load\n");
+        return nullptr;
+    }
+    auto &nvram_proxy_data_prop = *nvram_proxy_data_prop_ptr;
+    const auto proxyData = NVRAM::extractProxyData(nvram_proxy_data_prop.data());
+    const auto hasVerbose = proxyData.system_hasBootArg("-v");
+    printf("boot-args has -v flag: %d\n", hasVerbose);
+    if (!hasVerbose) {
+        printf("didn't detect verbose boot flag, bailing out of xnu load\n");
+        return nullptr;
+    }
+
+    auto memory_map_node_ptr = chosen_node.childNamed("memory-map");
+    if (!memory_map_node_ptr) {
+        printf("couldn't find chosen/memory-map, bailint out of xnu load\n");
+        return nullptr;
+    }
+    auto &memory_map_node = *memory_map_node_ptr;
+    // FMT_PRINT("memory_map_node: {}\n", memory_map_node);
+    for (const auto &map_region : memory_map_node.properties_sized(sizeof(DTRegister))) {
+        // FMT_PRINT("map_region: {}\n", map_region);
+        FMT_PRINT("map_region.as_reg(): {}\n", map_region.as_reg());
+    }
+    auto payload_prop_ptr = memory_map_node.propertyNamed("Kernel-PYLD");
+    if (!payload_prop_ptr) {
+        printf("couldn't find chosen/memory-map/Kernel-PYLD, bailing out of xnu load\n");
+        return nullptr;
+    }
+    auto &payload_prop = *payload_prop_ptr;
+    FMT_PRINT("payload_prop: {}\n", payload_prop.as_reg());
 
     const auto cpuImplRegAddrs = DT::getCPUImplRegAddrs(dt);
     for (const auto &cpuImplRegAddr : cpuImplRegAddrs) {
