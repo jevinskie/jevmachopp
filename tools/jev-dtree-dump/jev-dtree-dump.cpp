d4f07fb45fa9854c52c627139cc79a4e5caa1ebe
Jevin Sweval <jevinsweval@gmail.com>
2021-11-28 11:41:45 -0800

    initial DT path traversal skeleton

--- tools/jev-dtree-dump/jev-dtree-dump.cpp
+++ tools/jev-dtree-dump/jev-dtree-dump.cpp
@@ -1,13 +1,36 @@
-#include <assert.h>
-
 #include <jevmachopp/Slurp.h>
 #include <jevmachopp/c/jevdtree.h>
 
-int main(int argc, const char *argv[]) {
-    assert(argc == 2);
-    uint8_t *inbuf = Slurp::readfile(argv[1]);
+#include <cxxopts.hpp>
 
-    dump_dtree(inbuf);
+int main(int argc, const char *argv[]) {
+    cxxopts::Options options("jev-dtree-dump", "DeviceTree tool");
+
+    // clang-format off
+    options.add_options()
+        ("d,devicetree", "devicetree file", cxxopts::value<std::string>())
+        ("l,lookup", "lookup path in the device tree", cxxopts::value<std::vector<std::string>>())
+        ("h,help", "Print usage")
+    ;
+    // clang-format on
+
+    auto args = options.parse(argc, argv);
+    if (args.count("help")) {
+        puts(options.help().data());
+        return 1;
+    }
+
+    uint8_t *dtbuf = Slurp::readfile(args["devicetree"].as<std::string>().data());
+
+    dump_dtree(dtbuf);
+
+    printf("args.count(\"lookup\"): %zu\n", args.count("lookup"));
+    if (args.count("lookup")) {
+        const auto &lookups = args["lookup"].as<std::vector<std::string>>();
+        for (const auto &lookup : lookups) {
+            printf("lookup arg: %s\n", lookup.data());
+        }
+    }
 
     return 0;
 }
