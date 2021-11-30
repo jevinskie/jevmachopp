9b1e40e2ad1ab3def8cba1bf5ce1afd7c9345f6d
Jevin Sweval <jevinsweval@gmail.com>
2021-11-29 20:05:06 -0800

    wip

--- lib/jevmachopp/CallFinder.cpp
+++ lib/jevmachopp/CallFinder.cpp
@@ -6,12 +6,13 @@ namespace CallFinder {
 bool findCallsTo(const MachO &macho, const std::string_view symbol_name) {
     const SymtabCommand *symtab = macho.symtab();
     assert(symtab);
+    const auto dylib_name_map = macho.dylibNamesMap();
     const auto seg_name_map = macho.segmentNamesMap();
     const NList *symbol_nl_ptr = find_if_or_nullptr(macho.indirect_syms(), [&](const NList &nl) {
         return nl.name(macho, *symtab) == symbol_name && nl.segmentName(seg_name_map) == "__TEXT"sv;
     });
     if (symbol_nl_ptr) {
-        FMT_PRINT("symbol_nl: {:f}\n", *symbol_nl_ptr, macho);
+        FMT_PRINT("symbol_nl: {:m}\n", *symbol_nl_ptr, macho, fmt::ptr(&dylib_name_map));
     }
     int idx = 0;
     for (const auto &nl : macho.indirect_syms()) {
