9c6b9ac256e1399556cb14b75a43d26ec1e7ff7e
Jevin Sweval <jevinsweval@gmail.com>
2021-11-30 19:07:13 -0800

    maybe wip

--- include/jevmachopp/FunctionStartsCommand.h
+++ include/jevmachopp/FunctionStartsCommand.h
@@ -8,6 +8,128 @@
 #include "jevmachopp/MachO.h"
 #include "jevmachopp/SegmentCommand.h"
 
+// template <std::ranges::forward_range V>
+// requires requires() {
+//     requires std::ranges::view<V>;
+// }
+// class offset_partial_sum_view {
+//     V base_ = V();
+//     using B = decltype(base_.base());
+
+// public:
+//     offset_partial_sum_view() = default;
+//     offset_partial_sum_view(V b) : base_(std::move(b)) { }
+
+//     auto begin() -> std::ranges::iterator_t<V> { return ranges::begin(base_); }
+//     auto end() -> std::ranges::iterator_t<V> { return ranges::end(&base_); }
+// };
+
+// template <typename t>
+// using iterator_t = decltype(std::begin(std::declval<t &>()));
+
+// template <typename t>
+// using range_reference_t = decltype(*std::begin(std::declval<t &>()));
+
+template <typename urng_t>
+//     requires (bool)ranges::InputRange<urng_t>() &&
+//              (bool)ranges::CommonReference<range_reference_t<urng_t>, uint64_t>()
+class view_add_constant : public ranges::view_base {
+private:
+    /* data members == "the state" */
+    struct data_members_t {
+        urng_t urange;
+    };
+    std::shared_ptr<data_members_t> data_members;
+
+    /* the iterator type */
+    struct iterator_type : iterator_t<urng_t> {
+        using base = iterator_t<urng_t>;
+        using reference = uint64_t;
+
+        iterator_type() = default;
+        iterator_type(base const &b) : base{b} {}
+
+        iterator_type operator++(int) {
+            return static_cast<base &>(*this)++;
+        }
+
+        iterator_type &operator++() {
+            ++static_cast<base &>(*this);
+            return (*this);
+        }
+
+        reference operator*() const {
+            return *static_cast<base>(*this) + 42;
+        }
+    };
+
+public:
+    /* member type definitions */
+    using reference = uint64_t;
+    using const_reference = uint64_t;
+    using value_type = uint64_t;
+
+    using iterator = iterator_type;
+    using const_iterator = iterator_type;
+
+    /* constructors and deconstructors */
+    view_add_constant() = default;
+    constexpr view_add_constant(view_add_constant const &rhs) = default;
+    constexpr view_add_constant(view_add_constant &&rhs) = default;
+    constexpr view_add_constant &operator=(view_add_constant const &rhs) = default;
+    constexpr view_add_constant &operator=(view_add_constant &&rhs) = default;
+    ~view_add_constant() = default;
+
+    view_add_constant(urng_t &&urange)
+        : data_members{new data_members_t{std::forward<urng_t>(urange)}} {}
+
+    /* begin and end */
+    iterator begin() const {
+        return std::begin(data_members->urange);
+    }
+    iterator cbegin() const {
+        return begin();
+    }
+
+    auto end() const {
+        return std::end(data_members->urange);
+    }
+
+    auto cend() const {
+        return end();
+    }
+};
+
+template <typename urng_t>
+//     requires (bool)ranges::InputRange<urng_t>() &&
+//              (bool)ranges::CommonReference<range_reference_t<urng_t>, uint64_t>()
+view_add_constant(urng_t &&)->view_add_constant<urng_t>;
+
+// static_assert((bool)ranges::input_range<view_add_constant<std::vector<uint64_t>>>());
+// static_assert((bool)ranges::view<view_add_constant<std::vector<uint64_t>>>());
+
+struct add_constant_fn {
+    template <typename urng_t>
+    //         requires (bool)ranges::InputRange<urng_t>() &&
+    //                  (bool)ranges::CommonReference<range_reference_t<urng_t>, uint64_t>()
+    auto operator()(urng_t &&urange) const {
+        return view_add_constant{std::forward<urng_t>(urange)};
+    }
+
+    template <typename urng_t>
+    //         requires (bool)ranges::InputRange<urng_t>() &&
+    //                  (bool)ranges::CommonReference<range_reference_t<urng_t>, uint64_t>()
+    friend auto operator|(urng_t &&urange, add_constant_fn const &) {
+        return view_add_constant{std::forward<urng_t>(urange)};
+    }
+};
+
+namespace view {
+
+add_constant_fn constexpr add_constant;
+
+}
+
 class FunctionStartsCommand : public LinkeditDataCommand {
 public:
     raw_func_start_range raw_offsets(const MachO &macho) const;
