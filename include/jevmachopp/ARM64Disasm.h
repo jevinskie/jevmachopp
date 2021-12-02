5ec7e620285e3c2ddb388eeb64526c6c0e0b0373
Jevin Sweval <jevinsweval@gmail.com>
2021-12-01 21:20:21 -0800

    wip

--- include/jevmachopp/ARM64Disasm.h
+++ include/jevmachopp/ARM64Disasm.h
@@ -14,3 +14,16 @@ std::optional<bl_off> decodeBL(const inst_raw_t inst);
 bool isBLTo(const inst_raw_t inst, uint64_t pc, uint64_t target_addr);
 
 } // namespace ARM64Disasm
+
+class ARM64Inst {
+public:
+    ARM64Inst(uint32_t inst_word, uint64_t pc) : inst_word(inst_word), pc(pc) {}
+
+public:
+    bool isBL() const;
+    bool isBLTo(uint64_t target_addr) const;
+
+private:
+    const ARM64Disasm::inst_raw_t inst_word;
+    const uint64_t pc;
+};
--- lib/jevmachopp/ARM64Disasm.cpp
+++ lib/jevmachopp/ARM64Disasm.cpp
@@ -25,3 +25,13 @@ bool isBLTo(const inst_raw_t inst, uint64_t pc, uint64_t target_addr) {
 }
 
 } // namespace ARM64Disasm
+
+using namespace ARM64Disasm;
+
+bool ARM64Inst::isBL() const {
+    return ::isBL(inst_word);
+}
+
+bool ARM64Inst::isBLTo(uint64_t target_addr) const {
+    return ::isBLTo(inst_word, pc, target_addr);
+}
