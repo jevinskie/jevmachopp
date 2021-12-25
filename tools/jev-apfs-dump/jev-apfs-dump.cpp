6d70e2cfeca1bbbc6d05aa5a99a01c650fec3638
Jevin Sweval <jevinsweval@gmail.com>
2021-12-25 18:15:16 -0500

    add apfs-fuse submodule, initial jev-apfs-dump skeleton

---
+++ tools/jev-apfs-dump/jev-apfs-dump.cpp
@@ -0,0 +1,16 @@
+#include <cassert>
+#include <cstdint>
+#include <cstdio>
+
+
+#include <ApfsLib/Device.h>
+#include <ApfsLib/ApfsContainer.h>
+#include <ApfsLib/ApfsVolume.h>
+#include <ApfsLib/BlockDumper.h>
+#include <ApfsLib/ApfsDir.h>
+#include <ApfsLib/GptPartitionMap.h>
+
+int main(int argc, const char **argv) {
+    assert(argc == 2);
+    return 0;
+}
