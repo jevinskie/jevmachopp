7d0186e10e25f6fdb89c1b5af75c848f026386c1
Jevin Sweval <jevinsweval@gmail.com>
2022-01-01 15:15:29 -0500

    wip

--- tools/jev-apfs-dump/jev-apfs-dump.cpp
+++ tools/jev-apfs-dump/jev-apfs-dump.cpp
@@ -11,6 +11,15 @@
 #include <ApfsLib/GptPartitionMap.h>
 
 int main(int argc, const char **argv) {
-    assert(argc == 2);
+    if (argc != 4) {
+        fprintf(stderr, "usage: jev-apfs-dump <disk image> <Volume Group UUID (vuid)> <Next Stage Image4 Hash (nsih)>\n")
+        return 1;
+    }
+    const auto disk_img_path_cstr = argv[1];
+    const auto vuid_cstr = argv[2];
+    const auto nsih_cstr = argv[3];
+    std::size_t disk_buf_len = 0;
+    const auto disk_buf = readfile(disk_img_path_cstr, &disk_buf_len, false /* rw */, (const void *)0x400000000ull);
+
     return 0;
 }
