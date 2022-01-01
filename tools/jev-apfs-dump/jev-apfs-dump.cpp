a67485e16d280dc41030765ead2711d2fe3ab390
Jevin Sweval <jevinsweval@gmail.com>
2022-01-01 15:23:59 -0500

    jev-apfs-dump build fix

--- tools/jev-apfs-dump/jev-apfs-dump.cpp
+++ tools/jev-apfs-dump/jev-apfs-dump.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <cstdio>
 
+#include <jevmachopp/Slurp.h>
 
 #include <ApfsLib/Device.h>
 #include <ApfsLib/ApfsContainer.h>
@@ -12,14 +13,14 @@
 
 int main(int argc, const char **argv) {
     if (argc != 4) {
-        fprintf(stderr, "usage: jev-apfs-dump <disk image> <Volume Group UUID (vuid)> <Next Stage Image4 Hash (nsih)>\n")
+        fprintf(stderr, "usage: jev-apfs-dump <disk image> <Volume Group UUID (vuid)> <Next Stage Image4 Hash (nsih)>\n");
         return 1;
     }
     const auto disk_img_path_cstr = argv[1];
     const auto vuid_cstr = argv[2];
     const auto nsih_cstr = argv[3];
     std::size_t disk_buf_len = 0;
-    const auto disk_buf = readfile(disk_img_path_cstr, &disk_buf_len, false /* rw */, (const void *)0x400000000ull);
+    const auto disk_buf = Slurp::readfile(disk_img_path_cstr, &disk_buf_len, false /* rw */, (const void *)0x400000000ull);
 
     return 0;
 }
