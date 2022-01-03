c3a6af7cd4efd1c771b08e23450e547fce546f45
Jevin Sweval <jevinsweval@gmail.com>
2022-01-03 12:02:39 -0500

    wip

--- tools/jev-apfs-dump/jev-apfs-dump.cpp
+++ tools/jev-apfs-dump/jev-apfs-dump.cpp
@@ -74,6 +74,25 @@ unsigned long fake_block_read(struct blk_desc *block_dev, lbaint_t start, lbaint
     return blkcnt;
 }
 
+ApfsDir::DirRec *childDirNamed(ApfsDir *apfsDir, ApfsDir::DirRec *parentDir, std::string_view childDirName) {
+    return nullptr;
+}
+
+std::unique_ptr<ApfsDir::DirRec> lookupDir(ApfsDir *apfsDir, std::string_view dirPath) {
+    if (!dirPath.size() || dirPath[0] != '/') {
+        return nullptr;
+    }
+    std::unique_ptr<ApfsDir::DirRec> res = std::make_unique<ApfsDir::DirRec>();
+    assert(apfsDir->LookupName(*res, ROOT_DIR_PARENT, "root"));
+    for (const auto childName : stringSplitViewDelimitedBy(dirPath, '/')) {
+        res = std::unique_ptr<ApfsDir::DirRec>{childDirNamed(apfsDir, res.get(), childName)};
+        if (!res) {
+            return nullptr;
+        }
+    }
+    return res;
+}
+
 int main(int argc, const char **argv) {
     if (argc != 4) {
         fprintf(stderr, "usage: jev-apfs-dump <disk image> <Volume Group UUID (vuid)> <Next Stage "
@@ -118,15 +137,21 @@ int main(int argc, const char **argv) {
     assert(preboot_vol);
     fprintf(stderr, "preboot name: %s\n", preboot_vol->name());
 
-    std::filesystem::path kc_path{"D8961206-5EAC-4D35-94A3-5412F17E6B3B/boot/CBE5168B59E7B0104701733E3A617B82BC6F895B88CACFCE327725CB5532929C19244CFCEF709E3D0F8337A4866F608C/System/Library/Caches/com.apple.kernelcaches/kernel cache"};
+    std::string_view kc_dir{"/D8961206-5EAC-4D35-94A3-5412F17E6B3B/boot/CBE5168B59E7B0104701733E3A617B82BC6F895B88CACFCE327725CB5532929C19244CFCEF709E3D0F8337A4866F608C/System/Library/Caches/com.apple.kernelcaches"};
+    std::string_view kc_path{"/D8961206-5EAC-4D35-94A3-5412F17E6B3B/boot/CBE5168B59E7B0104701733E3A617B82BC6F895B88CACFCE327725CB5532929C19244CFCEF709E3D0F8337A4866F608C/System/Library/Caches/com.apple.kernelcaches/kernelcache"};
 
-    for (const auto &path_part : kc_path) {
-        fmt::print("path_part: {:s}\n", path_part.c_str());
-    }
-
-    ApfsDir dir(*preboot_vol);
+    ApfsDir apfsDir(*preboot_vol);
     ApfsDir::DirRec res;
-    assert(dir.LookupName(res, ROOT_DIR_INO_NUM, "D8961206-5EAC-4D35-94A3-5412F17E6B3B"));
+    assert(apfsDir.LookupName(res, ROOT_DIR_INO_NUM, "D8961206-5EAC-4D35-94A3-5412F17E6B3B"));
+
+    assert(apfsDir.LookupName(res, ROOT_DIR_PARENT, "root"));
+
+    const auto kc_dir_res = lookupDir(&apfsDir, kc_dir);
+    if (kc_dir_res) {
+        fmt::print("found dir named: {:s}\n", kc_dir_res->name);
+    } else {
+        fmt::print("failed to find dir\n");
+    }
 
     return 0;
 }
