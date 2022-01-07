73b2a48c19189b907807214f808755cf9140c8f0
Jevin Sweval <jevinsweval@gmail.com>
2022-01-07 10:08:02 -0800

    ugh

--- include/jevmachopp/UBootCompat.h
+++ include/jevmachopp/UBootCompat.h
@@ -8,6 +8,11 @@
 extern "C" {
 
 #define fputc __uboot_fputc
+#define putc __uboot_putc
+#define puts __uboot_puts
+#define fprintf __uboot_fprintf
+#define fputs __uboot_fputs
+#define fgetc __uboot_fgetc
 
 #include <linux/compiler_attributes.h>
 
@@ -16,6 +21,11 @@ extern "C" {
 #undef clamp
 
 #undef fputc
+#undef putc
+#undef puts
+#undef fprintf
+#undef fputs
+#undef fgetc
 
 
 }
