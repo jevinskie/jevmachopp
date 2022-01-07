bc124d37076c2b98e037b9f9e96c30b6cbc4330b
Jevin Sweval <jevinsweval@gmail.com>
2022-01-07 08:49:47 -0800

    iffy uboot compat wip

--- include/jevmachopp/UBootCompat.h
+++ include/jevmachopp/UBootCompat.h
@@ -1,7 +1,21 @@
 #pragma once
 
+#include "stdint.h"
+#include "stdio.h"
+
+#include "sys/cdefs.h"
+
 extern "C" {
 
+#define fputc __uboot_fputc
+
+#include <linux/compiler_attributes.h>
+
+#include <linux/kernel.h>
+#undef swap
+#undef clamp
+
+#undef fputc
 
 
-} // extern "C"
+}
